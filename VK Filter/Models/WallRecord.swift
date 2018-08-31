b15d5faf423ccd1e37525346fb20878d365613bb
Alexander Zakatnov <me@zaakk.com>
2018-09-01 00:24:59 +0300

    wall records models

--- VK Filter/Models/WallRecord.swift
+++ VK Filter/Models/WallRecord.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-enum PostType:String {
+enum PostType:String, Codable {
     case post
     case copy
     case reply
@@ -40,12 +40,109 @@ struct Reposts:Codable {
     }
 }
 
+enum AttachmentType:String, Codable {
+    case photo
+    case postedPhoto
+    case video
+    case audio
+    case doc
+    case graffiti
+    case link
+    case note
+    case app
+    case poll
+    case page
+    case album
+    case photosList
+    case market
+    case marketAlbum
+    case sticker
+    case prettyCards
+    
+    enum CodingKeys:String, CodingKey {
+        case photo, video, audio, doc, graffiti, link, note, app, poll, page, album, market, sticker
+        case postedPhoto = "posted_photo"
+        case photosList = "photos_list"
+        case marketAlbum = "market_album"
+        case prettyCards = "pretty_cards"
+    }
+}
+
+struct VideoAttachment:Codable {
+    let id:Int
+    let ownerId:Int
+    let title:String
+    let description:String
+    let duration:Int
+    let photo130:Int
+    let photo320:Int
+    let photo640:Int
+    let photo800:Int
+    let date:TimeInterval
+    let addingDate:TimeInterval
+    let views:Int
+    let comments:Int
+    let player:URL
+    let platform:String
+    let canEdit:Bool?
+    let canAdd:Bool?
+    let isPrivate:Bool?
+    let accessKey:String
+    let processing:Bool?
+    let live:Bool?
+    let upcoming:Bool?
+    
+    enum CodingKeys:String, CodingKey {
+        case id, title, description, duration, date, views, comments, player, platform, processing, live, upcoming
+        case ownerId = "owner_id"
+        case photo130 = "photo_130"
+        case photo320 = "photo_320"
+        case photo640 = "photo_640"
+        case photo800 = "photo_800"
+        case addingDate = "adding_date"
+        case canEdit = "can_edit"
+        case canAdd = "can_add"
+        case isPrivate = "is_private"
+        case accessKey = "access_key"
+    }
+}
+
+struct AudioAttachment:Codable {
+    let id:Int
+    let ownerId:Int
+    let artist:String
+    let title:String
+    let duration:Int
+    let url:URL
+    let lyricsId:Int
+    let albumId:Int
+    let genreId:Int
+    let date:TimeInterval
+    let noSearch:Bool?
+    let isHq:Bool
+    
+    enum CodingKeys:String, CodingKey {
+        case id, artist, title, duration, url, date
+        case ownerId = "owner_id"
+        case lyricsId = "lyrics_id"
+        case albumId = "album_id"
+        case genreId = "genre_id"
+        case noSearch = "no_search"
+        case isHq = "is_hq"
+    }
+}
+
+struct Attachment:Codable {
+    let type:AttachmentType
+    
+}
+
 class WallRecord: Codable {
     let id:Int
     let fromId:Int
     let ownerId:Int
     let date:Date
-    let postType:String //TODO: replace with enum
+    let postType:PostType //TODO: replace with enum
     let text:String
     let copyHistory:WallRecord?
     let canDelete:Bool
